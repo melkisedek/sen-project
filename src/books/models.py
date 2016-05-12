e8994725ce597bd545b1caf951444df8bed37143
Melkisedek Hamakali <melkisedek.hamakali@gmail.com>
2016-05-12 07:40:02 +0100

    Add the Books app
    Added the books app and created the Book model

---
+++ src/books/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+class Book(models.Model):
+	title = models.CharField(max_length=200)
+	author = models.CharField(max_length=200)
+	year = models.DateTimeField('year published')
+	pages = models.IntegerField(default=0)
+	isbn_10	= models.IntegerField(default=0)
+	isbn_13	= models.IntegerField(default=0)
+	description = models.TextField()
+	cover_image	= models.ImageField('Cover Image',
+                                upload_to='cover_pics/%Y-%m-%d/',
+                                null=True,
+                                blank=True)
+	add_date = models.DateTimeField('date added')	
+	def __str__(self):
+		return self.title + " by " + self.author
+
+	def was_added_recently(self):
+		return self.add_date >= timezone.now() - datetime.timedelta(days=30)
