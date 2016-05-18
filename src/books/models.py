5ad5bcef00c807dadd9c829f1eb459c4cc73cb5f
Melkisedek Hamakali <melkisedek.hamakali@gmail.com>
2016-05-18 13:18:08 +0100

    Refactor some book model variables
    Add an edition field, help texts to some of the fields.
    Change behaviour of the __str__ method to be more informative.

--- src/books/models.py
+++ src/books/models.py
@@ -5,12 +5,14 @@ from django.utils import timezone
 
 class Book(models.Model):
 	title = models.CharField(max_length=200)
+	edition = models.SmallIntegerField(default=1)
 	author = models.CharField(max_length=200)
 	year = models.DateTimeField('year published',
-		help_text="Please use the following format: <em>YYYY-MM-DD</em>.")
+		help_text="Please use the following format: <em>YYYY-MM-DD</em>.",
+		blank=True)
 	pages = models.IntegerField(default=0)
-	isbn_10	= models.IntegerField(default=0)
-	isbn_13	= models.IntegerField(default=0)
+	isbn_10	= models.IntegerField(default=0,help_text="Do not include dashes")
+	isbn_13	= models.IntegerField(default=0,help_text="Do not include dashes")
 	description = models.TextField()
 	cover_image	= models.ImageField('cover Image',
                                 upload_to='cover_pics/%Y-%m-%d/',
@@ -18,7 +20,14 @@ class Book(models.Model):
                                 blank=True)
 	date_added = models.DateTimeField(default=datetime.now)	
 	def __str__(self):
-		return self.title + " by " + self.author
+		if self.edition==1:
+			nth="st"
+		elif self.edition==2:
+			nth="nd"
+		elif self.edition==3:
+			nth="rd"
+		else : nth="th"
+		return self.title + ", "+ str(self.edition)+nth + " Edition by " + self.author
 
 	def was_added_recently(self):
 		return self.date_added >= timezone.now() - datetime.timedelta(days=30)
