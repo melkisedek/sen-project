0f639ce10c3ae4c38e8fdb429ff4772466ac203e
Melkisedek Hamakali <melkisedek.hamakali@gmail.com>
2016-06-08 15:25:13 +0100

    Enhance admin view of books
    Make it easier to find a book by searching in admin

--- src/books/admin.py
+++ src/books/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Book, Author, Publisher, Loaned
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+	list_display = ('name', 'date_added')
+	search_fields = ["name"]
+	ordering = ["name"]
 
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
 admin.site.register(Publisher)
 admin.site.register(Loaned)
